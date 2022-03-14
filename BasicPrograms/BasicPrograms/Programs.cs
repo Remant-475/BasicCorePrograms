683dc81a417ab85b31dd1332c5e8c7a376ba776b
remant <remantmahato9798@gmail.com>
2022-03-15 00:24:35 +0530

    Added: UC7-SwapTwoNumber

--- BasicPrograms/BasicPrograms/Programs.cs
+++ BasicPrograms/BasicPrograms/Programs.cs
@@ -4,26 +4,26 @@ namespace BasicPrograms
 {
      class Programs
     {
-        public void QuotientAndRemainder()
+        public void SwapNumber()
         {
-            Console.WriteLine("Enter the Number");
-            int Num = int.Parse(Console.ReadLine());
-            Console.WriteLine(" Number divided by");
-            int Divide = int.Parse(Console.ReadLine());
-            int Num1;
-            int Num2;
+            Console.WriteLine("Enter Number a : ");
+            int Num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number b :");
+            int Num2 = int.Parse(Console.ReadLine());
 
-            Num1 = (Num % Divide);
-            Console.WriteLine("Remainder of Number = " + Num1);
-            Num2 = (Num / Divide);
-            Console.WriteLine("Quotient of Number = " + Num2);
+            Num1 = Num1 * Num2;
+            Num2 = Num1 / Num2;
+            Num1 = Num1 / Num2;
+
+            Console.WriteLine("Number after Swapping \n");
+            Console.WriteLine("Number a :" + Num1 + " Number b :" + Num2);
+
 
         }
-
         public static void Main(String[]args)
         {
-            Programs quotient=new Programs();
-            quotient.QuotientAndRemainder();
+            Programs swap=new Programs();
+            swap.SwapNumber();
         }
     }
 }
