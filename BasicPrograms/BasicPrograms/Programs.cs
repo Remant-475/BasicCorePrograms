73e0d7bceb30fdf189648ec99a4cd89a813371db
remant <remantmahato9798@gmail.com>
2022-03-15 02:44:56 +0530

    Added: UC7-refactored

--- BasicPrograms/BasicPrograms/Programs.cs
+++ BasicPrograms/BasicPrograms/Programs.cs
@@ -7,16 +7,16 @@ namespace BasicPrograms
         public void SwapNumber()
         {
             Console.WriteLine("Enter Number a : ");
-            int Num1 = int.Parse(Console.ReadLine());
+            int Num_a = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Number b :");
-            int Num2 = int.Parse(Console.ReadLine());
+            int Num_b = int.Parse(Console.ReadLine());
 
-            Num1 = Num1 * Num2;
-            Num2 = Num1 / Num2;
-            Num1 = Num1 / Num2;
+            Num_a = Num_a * Num_b;
+            Num_b = Num_a / Num_b;
+            Num_a = Num_a / Num_b;
 
             Console.WriteLine("Number after Swapping \n");
-            Console.WriteLine("Number a :" + Num1 + " Number b :" + Num2);
+            Console.WriteLine("Number a :" + Num_a + " Number b :" + Num_b);
 
 
         }
